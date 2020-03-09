ee986ca33eddb1981b79946fc84223ac5548ce55
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-09 17:54:02 -0500

    fix uninitialized values

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,12 +7,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
-		return NULL;
-	new->n = value;
+	new = binary_tree_node(parent, value);
 
-	if (parent->right != NULL)
+	if (parent->right)
 	{
 		new->right = parent->right;
 		parent->right = new;
