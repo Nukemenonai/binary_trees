1c4a85462fbe76e8a431bb5e61610cbd3a993b1e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-09 11:37:20 -0500

    first task

--- binary_trees.h
+++ binary_trees.h
@@ -9,6 +9,9 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
+
+#include <stddef.h>
+
 struct binary_tree_s
 {
 	int n;
@@ -18,11 +21,17 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
 typedef struct binary_tree_s bst_t;
-
 typedef struct binary_tree_s avl_t;
-
 typedef struct binary_tree_s heap_t;
+
+/* print declarations */
+
+void binary_tree_print(const binary_tree_t *tree);
+
+
+/* project functions declarations */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
