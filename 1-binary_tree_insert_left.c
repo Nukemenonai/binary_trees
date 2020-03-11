64bb5c7a851cd13ddf69c43a25a65fa1e873b676
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-11 15:18:35 -0500

    fix: check for failed malloc

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new = binary_tree_node(parent, value);
 
-	if (!parent)
+	if (!parent || !new)
 		return (NULL);
 	if (parent->left)
 	{
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new = binary_tree_node(parent, value);
 
-	if (!parent)
+	if (!parent || !new)
 		return (NULL);
 	if (parent->right)
 	{
