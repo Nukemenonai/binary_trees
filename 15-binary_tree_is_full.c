f75a9994ae0f51e4be14793a27f26246abd966a5
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-11 18:50:00 -0500

    fix: betty format

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_full - checks if a node has all its childs full
+ *
+ * @tree: the porinter to the node to eval
+ * Return: 1 if true 0 if not
+ */
+
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int left = 0, right = 0;
@@ -10,7 +17,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (!tree->right && !tree->left)
 		return (1);
-        left = binary_tree_is_full(tree->left);
+	left = binary_tree_is_full(tree->left);
 	right = binary_tree_is_full(tree->right);
 	return (left && right ? 1 : 0);
 }
