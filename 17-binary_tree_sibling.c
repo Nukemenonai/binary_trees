fbb81f0d7ecab5c61602948cc358a22a8076760b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-12 12:29:25 -0500

    files for task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling
+ *
+ * @node: the node to evaluate
+ * Return: Always 0 (Success)
+ */
+
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *tmp;
+	binary_tree_t *sibling;
+
+	if (!node || !node->parent)
+		return (NULL);
+	tmp = node->parent;
+	sibling = (tmp->left == node ? tmp->right : tmp->left);
+	return (sibling == NULL ? NULL : sibling);
+}
