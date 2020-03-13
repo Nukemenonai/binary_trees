7fe5163910d4c5980ca6a76cb2faebe9ded9bb11
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-13 09:56:18 -0500

    fix : free

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,7 +17,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new = binary_tree_node(parent, value);
 
 	if (!parent || !new)
+	{
+		free(new);
 		return (NULL);
+	}
 	if (parent->left)
 	{
 		new->left = parent->left;
